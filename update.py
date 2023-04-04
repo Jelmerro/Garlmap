d94f97bb2bd001d6d4f1c77956eb461aba8d53d9
Jelmer van Arnhem <Jelmerro@users.noreply.github.com>
2023-04-04 23:58:45 +0200

    remove publish option from electron builder

--- update.py
+++ update.py
@@ -1,5 +1,5 @@
 # Garlmap - Gapless Almighty Rule-based Logical Mpv Audio Player
-# Copyright (C) 2021-2022 Jelmer van Arnhem
+# Copyright (C) 2021-2023 Jelmer van Arnhem
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,6 +20,7 @@ import subprocess
 import re
 
 overrides = {
+    "electron": "beta",
     "electron-builder": "next",
     "music-metadata": "cjs"
 }
